993545a4b7b594d2ba10388837b7c429cbc32b0c
redbeansoupy <victorialam2003@gmail.com>
2024-10-02 13:58:09 -0700

    wcat implemented

---
+++ project1/initial-utilities/wcat/wcat.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    // Validate arguments
+    if (argc <= 1) {
+        exit(0);
+    } 
+
+    for (int i = 1; i < argc; i++) {
+        // Open the file and check validity
+        int fileDescriptor = open(argv[i], O_RDONLY);
+        if (fileDescriptor < 0) {
+            cerr << "wcat: cannot open file" << endl;
+            exit(1);
+        } else if (fileDescriptor == 1) {
+            cerr << "Error calling open()" << endl;
+            exit(1);
+        }
+
+        // Load file data into a buffer. Print as it is being read
+        int ret = 0;
+        int bytesRead = 0;
+        char buffer[4096];
+        while ((ret = read(fileDescriptor, buffer, sizeof(buffer))) > 0) {
+            bytesRead = ret;
+            write(STDOUT_FILENO, buffer, bytesRead); // sizeof buffer is in bytes
+        }
+
+        if (fileDescriptor != STDIN_FILENO) {
+            close(fileDescriptor);
+        }
+    }
+    
+    return 0;
+}
